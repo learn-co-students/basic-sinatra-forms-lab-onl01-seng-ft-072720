59a75b55f6eafe33f766351499a17cb4c0d45e1b
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-09 11:06:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/' do
+        "Welcome, bitches!"
+    end
+
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @name = capitalize_all(params[:name])
+        @coach = capitalize_all(params[:coach])
+        @pg = capitalize_all(params[:pg])
+        @sg = capitalize_all(params[:sg])
+        @sf = capitalize_all(params[:sf])
+        @pf = capitalize_all(params[:pf])
+        @c = capitalize_all(params[:c])
+        erb :team
+    end
+
+    def capitalize_all(string)
+        string.split(" ").each{|word| word.capitalize!}.join(" ")
+    end
+
 
 end
